1e768cefd2506ba0adb40b490c0803362a36df5d
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-04-03 16:53:30 +0100

    fixed github link

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -35,9 +35,9 @@ export const projectDetails = [
         project_desc: `ChessLab offers a comprehensive training experience that enables you to study and refine your openings, as well as sharpen your tactical prowess. 
         This user-friendly mobile app provides a platform to create and edit your personalized opening repertoires, in addition to a multitude of puzzles of varying levels of difficulty. 
         By integrating the Spaced Repetition System (SRS), ChessLab ensures that your training adapts to your progress, maximizing memory retention and skill improvement.`,
-        tech_stack: ['React Native', 'Python', 'Firebase'],
+        tech_stack: ['Flutter', 'Python', 'Firebase'],
         project_img: ChessLabPrev,
-        project_url: 'https://github.com/MartimBaltazar/ChessLab',
+        // project_url: '#',
         reverse: false,
         button_text: 'Visit Website',
         button_url: 'http://www.chesslab.pt'
