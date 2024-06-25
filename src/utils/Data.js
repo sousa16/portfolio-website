051e5cf3b7d88379ee83df38067d54e531f73288
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-06-25 16:15:37 +0100

    updated deps and chesslab description

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -35,7 +35,7 @@ export const projectDetails = [
         project_desc: `ChessLab offers a comprehensive training experience that enables you to study and refine your openings, as well as sharpen your tactical prowess. 
         This user-friendly mobile app provides a platform to create and edit your personalized opening repertoires, in addition to a multitude of puzzles of varying levels of difficulty. 
         By integrating the Spaced Repetition System (SRS), ChessLab ensures that your training adapts to your progress, maximizing memory retention and skill improvement.`,
-        tech_stack: ['Flutter', 'Python', 'Firebase'],
+        tech_stack: ['React Native', 'Python', 'Firebase'],
         project_img: ChessLabPrev,
         // project_url: '#',
         reverse: false,
