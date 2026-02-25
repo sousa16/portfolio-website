809600cbc4cae8190a8486df419f9a63460ea448
sousa16 <joao.p.c.sousa@tecnico.ulisboa.pt>
2026-02-25 11:04:47 +0000

    fix: 404 on /blog

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/joaosousadev.pt",
+  base: "/",
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
